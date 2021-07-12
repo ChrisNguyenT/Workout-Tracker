0b359af01d9c9de37ec4170619b62af0ce875a4e
Thangnt <thangnt336@gmail.com>
2021-07-12 00:56:13 -0400

    editted server with mongoose connection and added models

--- Develop/server.js
+++ Develop/server.js
@@ -1,3 +1,21 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const logger = require("morgan");
+
+const PORT = process.env.PORT || 3100
+
+const app = express();
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(logger("dev"));
+
+app.use(express.static("public"));
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/Workout", {
+  useNewUrlParser: true,
+  useFindAndModify: false
+});
+
+app.listen(PORT, () => {
+  console.log(`App running on port ${PORT}!`);
+}); 
