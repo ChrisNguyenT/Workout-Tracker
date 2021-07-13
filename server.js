c5762c3aa3d3c6f949c2d88b6bfad207551dd8b4
Thangnt <thangnt336@gmail.com>
2021-07-12 20:32:20 -0400

    added require in server.js

--- server.js
+++ server.js
@@ -16,6 +16,9 @@ mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/Workout", {
   useFindAndModify: false
 });
 
+app.use(require("./routes/api.js"));
+app.use(require("./routes/html.js"));
+
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
 }); 
